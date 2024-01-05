382314e56cb8f4b944979845329aa32452ff8600
Fadwa Ali <fadwa.ali@elevoro.com>
2024-01-05 17:03:32 +0300

    fix style

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
   <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
-    <NavBar/>
-<RouterView/>
+    <NavBar />
+    <RouterView />
   </div>
 </template>
 
 <script setup lang="ts">
-import { RouterView } from 'vue-router';
-import NavBar from './components/NavBar.vue'
+import { RouterView } from "vue-router";
+import NavBar from "./components/NavBar.vue";
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
