baf1780abcadbcc156bc81f1d1c372edd70591c0
Fadwa Ali <fadwa.ali@elevoro.com>
2024-01-05 17:13:42 +0300

    create reusable component

---
+++ src/components/Modal.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8" :show="isOpen"
+  >
+    <div class="p-4 bg-white self-start mt-32 max-w-screen-md">
+      <slot />
+      <button class="text-white bg-weather-primary py2 px-6 rounded-md" @click="closeHandler">
+        Close
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  closeHandler?: any;
+  isOpen: boolean;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<style scoped></style>
