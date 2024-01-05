711890fbb33509a5fd5d36a634c956ac18808429
Fadwa Ali <fadwa.ali@elevoro.com>
2024-01-05 16:12:21 +0300

    create navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <header class="bg-weather-primary shadow-lg">
+        <nav>
+            <div>
+                <p>The Weather</p>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
