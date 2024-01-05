337737621338da05d47508271808c30e71f59ecf
Fadwa Ali <fadwa.ali@elevoro.com>
2024-01-05 17:03:13 +0300

    add router to nave bar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,17 +1,22 @@
 <template>
     <header class="bg-weather-primary shadow-lg sticky top-0">
         <nav class="container flex flex-col sm:flex-row items-center text-white py-6 gap-4">
-            <div class="flex flex-1 items-center gap-3">
-                <i class="fa-solid fa-sun text-2xl"></i>
-                <p class="text-2xl">The Weather</p>
+            <RouterLink :to="{ name: 'home' }">
+                <div class="flex items-center gap-3">
+                    <i class="fa-solid fa-sun text-2xl"></i>
+                    <p class="text-2xl">The Weather</p>
+                </div>
+            </RouterLink>
+            <div class="flex gap-3 justify-end flex-1"><i
+                    class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
+                <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
             </div>
-            <div><i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i></div>
         </nav>
     </header>
 </template>
 
 <script setup lang="ts">
-
+import { RouterLink } from 'vue-router';
 </script>
 
 <style scoped></style>
