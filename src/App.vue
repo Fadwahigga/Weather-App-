353312e4eacb8d8e4d33c2ce12986056052459d8
Fadwa Ali <fadwa.ali@elevoro.com>
2024-01-05 16:12:07 +0300

    impoet nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <NavBar/>
 <RouterView/>
   </div>
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
+import NavBar from './components/NavBar.vue'
 </script>
 
 <style scoped>
