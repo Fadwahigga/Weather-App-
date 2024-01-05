27dd8b890d26bc83bb5079f7d519081c450deed3
Fadwa Ali <fadwa.ali@elevoro.com>
2024-01-05 17:50:26 +0300

    feat: style

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,14 +1,20 @@
 <template>
-  <div
-    class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8" v-if="isOpen"
+  <Teleport to="body">
+    <div
+      class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
+      v-if="isOpen"
+    >
+      <div class="p-4 bg-white self-start mt-32 max-w-screen-md rounded-lg">
+        <slot />
+        <button
+          class="text-white bg-weather-primary py-2 px-6 rounded-md mt-4"
+          @click="closeHandler"
+        >
+          Close
+        </button>
+      </div>
+    </div></Teleport
   >
-    <div class="p-4 bg-white self-start mt-32 max-w-screen-md rounded-lg">
-      <slot />
-      <button class="text-white bg-weather-primary py-2 px-6 rounded-md mt-4" @click="closeHandler">
-        Close
-      </button>
-    </div>
-  </div>
 </template>
 
 <script setup lang="ts">
