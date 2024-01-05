d3dc61d0f5fb83811f3bb4fce96ca71ba62b63c4
Fadwa Ali <fadwa.ali@elevoro.com>
2024-01-05 17:37:41 +0300

    add radios button

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -2,9 +2,9 @@
   <div
     class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8" v-if="isOpen"
   >
-    <div class="p-4 bg-white self-start mt-32 max-w-screen-md">
+    <div class="p-4 bg-white self-start mt-32 max-w-screen-md rounded-lg">
       <slot />
-      <button class="text-white bg-weather-primary py-2 px-6 rounded-md" @click="closeHandler">
+      <button class="text-white bg-weather-primary py-2 px-6 rounded-md mt-4" @click="closeHandler">
         Close
       </button>
     </div>
