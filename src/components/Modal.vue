c0a0cc2edc26c48a97efe55469dfa7cd57841852
Fadwa Ali <fadwa.ali@elevoro.com>
2024-01-05 17:35:40 +0300

    fix show modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,10 +1,10 @@
 <template>
   <div
-    class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8" :show="isOpen"
+    class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8" v-if="isOpen"
   >
     <div class="p-4 bg-white self-start mt-32 max-w-screen-md">
       <slot />
-      <button class="text-white bg-weather-primary py2 px-6 rounded-md" @click="closeHandler">
+      <button class="text-white bg-weather-primary py-2 px-6 rounded-md" @click="closeHandler">
         Close
       </button>
     </div>
@@ -13,7 +13,7 @@
 
 <script setup lang="ts">
 interface Props {
-  closeHandler?: any;
+  closeHandler: any;
   isOpen: boolean;
 }
 
