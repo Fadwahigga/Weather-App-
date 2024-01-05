e54edfa94511a43cb6e6cce302df880692423baf
Fadwa Ali <fadwa.ali@elevoro.com>
2024-01-05 16:23:30 +0300

    feat: add icons

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,11 @@
 <template>
-    <header class="bg-weather-primary shadow-lg">
-        <nav>
-            <div>
-                <p>The Weather</p>
+    <header class="bg-weather-primary shadow-lg sticky top-0">
+        <nav class="container flex flex-col sm:flex-row items-center text-white py-6 gap-4">
+            <div class="flex flex-1 items-center gap-3">
+                <i class="fa-solid fa-sun text-2xl"></i>
+                <p class="text-2xl">The Weather</p>
             </div>
+            <div><i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i></div>
         </nav>
     </header>
 </template>
